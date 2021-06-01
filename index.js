814b831c3da612f206ed19b6839dad913137aac6
stepney141 <stepney141andi.c.i@gmail.com>
2021-06-01 12:11:44 +0900

    スクレイピング機能の実装

--- index.js
+++ index.js
@@ -9,31 +9,47 @@ const loyola_xpath = {
     link_from_home_to_board_menu: '//div[@id="tab-kj"]', //ホーム画面ヘッダーの掲示板アイコン
     link_from_home_to_board: '//*[@id="tabmenu-ul"]/li[1]/span', //掲示板アイコンをクリックした先から掲示板本体へ飛ぶリンク
     link_from_main_board_to_univ_bulletin_board: '/html/body/table[3]/tbody/tr[7]/td[1]/a', //掲示板ホームから大学掲示板へのリンク
-    club_info_in_main_board: "//td[contains(*, '課外活動')]/p[2]/a", //大学掲示板の中にある課外活動情報へのリンク
+    club_info_links: "//td[contains(*, '課外活動')]/p[2]/a", //大学掲示板の中にある課外活動情報へのリンク
+    club_info_update_times: '//*[@id="keijiSearchForm"]/table[4]/tbody/tr/td[4]', //課外活動の情報の更新日時（すべて）
+    club_info_update_time_most_recent: '//*[@id="keijiSearchForm"]/table[4]/tbody/tr[1]/td[4]', //課外活動の情報の更新日時（最新のみ）
     notice_title: "//span[@class='keiji-title']", //掲示タイトル
     notice_description: "//div[@class='keiji-naiyo']", //掲示内容
     notice_date: "//table[2]/tbody/tr[2]/td", //掲載日時
     notice_files: "//table[3]/tbody/tr/td/a", //すべての添付ファイルへのリンク群
 };
 
-const fetch_file = async (x, y, time, page) => {
+const mouse_click = async (x, y, page) => {
     try {
-        let statusCode = 0;
+        await Promise.all([
+            page.mouse.move(x, y),
+            page.waitForTimeout(1000),
+            page.mouse.click(x, y),
+        ]);
+        return true;
+    } catch (e) {
+        const error_m = 'mouse_click_error:';
+        console.error(error_m + e);
+        return false;
+    }
+};
+
+const fetch_file = async (x, y, page) => {
+    try {
+        let status_code = 0;
         await page._client.send('Page.setDownloadBehavior', {
             behavior: 'allow',
             downloadPath: setting.download_path
         });
         page.on('response', response => {
-            statusCode = response.status(); // HTTPステータスコードを取得する
+            status_code = response.status(); // HTTPステータスコードを取得する
         });
-        await mouse_click(x, y, time);
+        await mouse_click(x, y, page);
         await page.waitForTimeout(1000);
-        if (statusCode == 500) {
-            throw new Error('[StatusCode:500]ダウンロードできません');
+        if (status_code == 500) {
+            throw new Error('[status_code:500] ダウンロードできません');
         }
     } catch (e) {
-        const error_m = 'CSVダウンロード_エラー:';
-        console.error(error_m + e);
+        console.error(e);
         return false;
     }
     
@@ -44,12 +60,21 @@ const fetch_file = async (x, y, time, page) => {
         }
         return filename;
     })());
+
     return true;
+};
+
+const post_webhook = async () => {
+    
 };
 
 const loyola_scraper = async (browser) => {
     try {
         const page = await browser.newPage();
+        await page.evaluateOnNewDocument(() => { //webdriver.navigatorを消して自動操縦であることを隠す
+            Object.defineProperty(navigator, 'webdriver', ()=>{});
+            delete navigator.__proto__.webdriver;
+        });
 
         await page.goto(setting.loyola_uri, {
             waitUntil: "networkidle0",
@@ -68,44 +93,52 @@ const loyola_scraper = async (browser) => {
             waitUntil: "networkidle0",
         });
 
-        const linkToBoardMenuHandle = page.$x(loyola_xpath.link_from_home_to_board_menu);
-        await (await linkToBoardMenuHandle)[0].click(); //掲示板メニューを開く
+        await mouse_click(600, 100, page); //掲示板メニューを開く
 
-        await page.waitForNavigation({ //画面遷移待ち
-            timeout: 60000,
-            waitUntil: "networkidle0",
+        await mouse_click(50, 140, page); //掲示板本体へ飛ぶ（新規タブが開く）
+
+        /*
+        target="_blank"で開いた新規タブへの切り替え
+        ref: https://github.com/puppeteer/puppeteer/issues/3718#issuecomment-451325093
+        */
+        const pageTarget = page.target(); //新規タブのopenerを保存
+        const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget); //新規タブが開いたか確認
+        const newPage = await newTarget.page(); //新規タブの情報を保存
+        await newPage.evaluateOnNewDocument(() => { //webdriver.navigatorを消して自動操縦であることを隠す
+            Object.defineProperty(navigator, 'webdriver', ()=>{});
+            delete navigator.__proto__.webdriver;
         });
+        await newPage.waitForSelector("body"); //新規タブの画面遷移待ち
 
-        const linkToBoardHandle = page.$x(loyola_xpath.link_from_home_to_board);
-        await (await linkToBoardHandle)[0].click(); //掲示板本体へ飛ぶ
+        await mouse_click(40, 380, newPage); //詳細検索
+        await newPage.select('select#category1', '12'); //カテゴリ1の「学生生活」を選択
+        await newPage.select('select#category2', '16'); //カテゴリ2の「課外活動」を選択
+        await mouse_click(25, 230, newPage); //検索ボタンを押す
+        await newPage.waitForTimeout(2000);//画面遷移待ち
 
-        const linkToUnivBulletinBoardHandle = page.$x(loyola_xpath.link_from_main_board_to_univ_bulletin_board);
-        await (await linkToUnivBulletinBoardHandle)[0].click(); //大学掲示板へ飛ぶ
-
-        await page.waitForNavigation({ //画面遷移待ち
-            timeout: 60000,
-            waitUntil: "networkidle0",
-        });
+        const clubInfoTimesHandle = await newPage.$x(loyola_xpath.club_info_update_times);
+        for (const data of clubInfoTimesHandle) {
+            console.log(
+                await (await data.getProperty("innerText")).jsonValue()
+            );
+        }
 
     } catch (e) {
         console.log(e);
-        await browser.close();
         return false;
     }
     return true; 
 };
 
 (async () => {
-    
     const browser = await puppeteer.launch({
         defaultViewport: { width: 1000, height: 1000 },
         // headless: true,
         headless: false,
-        // slowMo: 100
+        slowMo: 50
     });
 
     await loyola_scraper(browser);
 
     await browser.close();
-
 })();
